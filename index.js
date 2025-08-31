e8f6f0c50ae80466046f5207b9a54635f2926a04
amanstep <aman@stepsecurity.io>
2025-08-31 21:28:12 +0530

    feat: added subscription check code

--- index.js
+++ index.js
@@ -2,12 +2,31 @@
 
 const core = require("@actions/core");
 const { context, GitHub } = require("@actions/github");
+const axios = require("axios")
+
+async function validateSubscription() {
+  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`
+
+  try {
+    await axios.get(API_URL, {timeout: 3000})
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 403) {
+      core.error(
+        'Subscription is not valid. Reach out to support@stepsecurity.io'
+      )
+      process.exit(1)
+    } else {
+      core.info('Timeout or API not reachable. Continuing to next step.')
+    }
+  }
+}
 
 /**
  * Main execution function for the pull request comment trigger action
  */
 async function execute() {
     try {
+        await validateSubscription();
         const triggerWord = core.getInput("trigger", { required: true });
         const reactionType = core.getInput("reaction");
         const strictPrefixMode = core.getInput("prefix_only") === 'true';
